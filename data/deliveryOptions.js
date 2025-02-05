31cddc76f8f5e67cc3d42b0079adcb0a636a9c25
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-02-05 11:19:31 +0300

    Updates On Amazon

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -16,11 +16,11 @@ export const deliveryOptions = [{
 export function getDeliveryOption(deliveryOptionId) {
     let deliveryOption;
 
-      deliveryOptions.forEach((option) =>{
+      deliveryOptions.forEach((option) => {
         if (option.id === deliveryOptionId) {
           deliveryOption = option;
         }
       });
 
       return deliveryOption || deliveryOptions[0];
-}
+};
--- data/products.js
+++ data/products.js
@@ -8,7 +8,7 @@ export function getProduct(productId){
       });
 
       return matchingProduct;
-}
+};
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
